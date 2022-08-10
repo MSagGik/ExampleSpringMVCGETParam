6bd058e24d7143c98108bd5ae5a3ed4e4c51b15e
MSagGik <Sagaciyang@yandex.ru>
2022-08-10 17:27:33 +0300

    Commit 100822

---
+++ src/main/java/ru/msaggik/spring/controllers/FirstController.java
@@ -0,0 +1,28 @@
+package ru.msaggik.spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/first") // для задания у класса уникального ветвления url
+public class FirstController {
+
+    @GetMapping("/hello")
+    public String helloPage(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+        System.out.println("Hello, " + name + " " + surname);
+        return "first/hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodByePage(@RequestParam(value = "name", required = false) String name,
+                              @RequestParam(value = "surname", required = false) String surname) {
+        System.out.println("GoodBye, " + name + " " + surname);
+        return "first/goodbye";
+    }
+}
